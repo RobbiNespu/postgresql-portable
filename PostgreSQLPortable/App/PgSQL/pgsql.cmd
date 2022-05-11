ef498d7419089526436077333bb30e061e049088
Robbi Nespu <robbinespu@gmail.com>
2022-05-11 08:18:43 +0800

    ❌ not working (postgresql 14.2-.2) . but uploaded for future references

--- PostgreSQLPortable/App/PgSQL/pgsql.cmd
+++ PostgreSQLPortable/App/PgSQL/pgsql.cmd
@@ -5,6 +5,28 @@ cls
 :: set default code page
 chcp 1252 > nul
 
+set PGDATABASE=postgres
+set PGPORT=5432
+set PGUSER=postgres
+set PGHOST=localhost
+set PGHOSTADDR=127.0.0.1
+set PGPASSWORD=postgres
+set PGSQL=%PAL:AppDir%\PgSQL
+set PGDATA=%PAL:DataDir%\data
+set PGLOG=%PAL:DataDir%\log.txt
+set PGLOCALEDIR=%PAL:AppDir%\PgSQL\share
+set PATH=%PATH%;%PAL:AppDir%\PgSQL\bin
+set PATH=%PATH%;%PAL:AppDir%\Perl\bin
+
+echo.
+echo Running on port %PGPORT%.
+
+set PAL=%CD%
+echo.
+cd ..
+cd data
+echo Running on path %CD%.
+
 :: initialise a new database on first use
 if not exist "%PGDATA%" (
 	echo.
@@ -15,10 +37,4 @@ if not exist "%PGDATA%" (
 :: startup postgres server
 echo.
 "%PGSQL%\bin\pg_ctl" -D "%PGDATA%" -l "%PGLOG%" -w start
-#cls
-#echo.
-#echo Running on port %PGPORT%. Type \q to quit and shutdown the server.
-#echo.
-#"%PGSQL%\bin\psql.exe" --port=%PGPORT% --dbname="%PGDATABASE%" --username="%PGUSER%"
-#echo.
-#"%PGSQL%\bin\pg_ctl" -D "%PGDATA%" stop
+
